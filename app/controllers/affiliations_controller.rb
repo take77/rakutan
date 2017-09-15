d6a8fa90d2ce706c8175d5c18da7a4f29763e142
masakazu takeda <teke77@toki.waseda.jp>
2017-09-15 17:48:54 +0900

    affiliation記入欄を作成。 同時にヘッダーを一部修正。

--- app/controllers/affiliations_controller.rb
+++ app/controllers/affiliations_controller.rb
@@ -1,5 +1,17 @@
 class AffiliationsController < ApplicationController
 
   def new
+    @affiliation = Affiliation.new
+  end
+
+  def create
+    affiliation = Affiliation.where(:college => params[:college], :department => params[:department], :course => params[:course]).first_or_create(create_params)
+    current_user.update_attributes(affiliation_id: affiliation.id)
+    redirect_to controller: :top, action: :index
+  end
+
+  private
+  def create_params
+    params.require(:affiliation).permit(:college, :department, :course)
   end
 end
